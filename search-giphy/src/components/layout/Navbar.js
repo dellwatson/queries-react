75b54c435d4ea87f370ed9d60501f230b19cb922
Dale Watson <dellryuzi@gmail.com>
2019-01-12 10:43:54 +0700

    basic component

---
+++ search-giphy/src/components/layout/Navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+  state = {
+    textInput : ""
+  }
+
+  
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Navbar;
